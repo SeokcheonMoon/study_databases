12ccc3c99de41d2a8c6fe5be26fde6f4e2dea4b5
seokcheon <my980519@gmail.com>
2023-12-14 09:25:51 +0900

    mongo insert

--- docs/NoSQL/mongo_inserts.py
+++ docs/NoSQL/mongo_inserts.py
@@ -1,7 +1,21 @@
-# mongoDB에 접속
+from pymongo import MongoClient
+
+# mongoDB에 접속 ( 자원에 대한 class)
+mongoClient = MongoClient("mongodb://localhost:27017") #해당하는 mongoDB의 주소를 변수에 담아준다.
+
 
 # database 생성 및 연결
+database = mongoClient["local"]
+
+
 
 # collection에 작업
+collection = database["fruits"]
+
 
 # insert 작업 진행
+
+collection.insert_one({"name": "오렌지", "color": "주황", "origin": "미국", "price": "2500"})
+
+
+pass
